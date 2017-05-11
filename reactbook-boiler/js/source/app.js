767be3224c9503ec3400a4e1cd7c926198138bd7
kakts <kakts.git@gmail.com>
2017-05-12 01:35:46 +0900

    Add browserify and babel to build reactbook-boiler

--- reactbook-boiler/js/source/app.js
+++ reactbook-boiler/js/source/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import React form 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
 
 ReactDOM.render(
   <h1>
-    <Logo /> アプリケーションにようこそ
+    <Logo /> アプリケーションにようこそ1
   </h1>,
   document.getElementById('app')
 );
