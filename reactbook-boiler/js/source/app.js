587b3483d108316a567a98bc5844d7b5906874fe
kakts <kakts.git@gmail.com>
2017-05-12 01:17:07 +0900

    Add template react-app

---
+++ reactbook-boiler/js/source/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React form 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+ReactDOM.render(
+  <h1>
+    <Logo /> アプリケーションにようこそ
+  </h1>,
+  document.getElementById('app')
+);
---
+++ reactbook-boiler/js/source/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Logo extends React.Component {
+  someMethod() {
+
+  }
+
+  another() {
+
+  }
+
+  render() {
+    return <div className="Logo" />;
+  }
+};
+export default Logo;
