60634a60d510c8c1271011e8fda758ef5e53db7c
kakts <kakts.git@gmail.com>
2017-05-28 21:39:53 +0900

    Add winepad2

---
+++ winepad2/scripts/build.sh
@@ -0,0 +1,8 @@
+# js transform
+babel js/source -d js/build
+# js package
+browserify js/build/app.js -o bundle.js
+# css package
+cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
+# done
+date; echo;
